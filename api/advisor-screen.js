0cb011778903e03be598750d39c96af262b943a1
Megsmileface1 <hartmanstanley@gmail.com>
2026-08-29 12:47:45 -0400

    Handle advisor screening timeout

--- api/advisor-screen.js
+++ api/advisor-screen.js
@@ -7,6 +7,7 @@ const paymentId = req.query.paymentId || "";
         input="dtmf"
         numDigits="1"
         timeout="5"
+        actionOnEmptyResult="true"
         action="https://scamcheck-lac.vercel.app/api/advisor-screen-result?paymentId=${encodeURIComponent(paymentId)}"
         method="POST"
       >
