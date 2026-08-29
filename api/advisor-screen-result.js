aea62be2dbdbb1117036198f9882bca32a4f125c
Megsmileface1 <hartmanstanley@gmail.com>
2026-08-29 12:23:34 -0400

    Read payment ID in advisor decision

--- api/advisor-screen-result.js
+++ api/advisor-screen-result.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   res.setHeader("Content-Type", "text/xml");
 
   const digits = req.body.Digits || "";
-
+const paymentId = req.query.paymentId || "";
   if (digits === "1") {
     return res.status(200).send(`
       <Response>
