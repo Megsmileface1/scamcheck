b7176b1c1599d20fa910bd7b669375d422ef9eb5
Megsmileface1 <hartmanstanley@gmail.com>
2026-08-29 12:54:30 -0400

    Authorize saved card before consultation

--- api/charge-saved-card.js
+++ api/charge-saved-card.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
             currency: "USD"
           },
           customer_id: squareCustomerId,
-          autocomplete: true,
+         autocomplete: false,
           note: "ScamCheck returning customer consultation"
         })
       }
