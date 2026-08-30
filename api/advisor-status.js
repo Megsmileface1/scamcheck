30b77b174756a09d179447cb96efbccdde39580b
Megsmileface1 <hartmanstanley@gmail.com>
2026-08-30 10:38:37 -0400

    Fix message after completed advisor call

--- api/advisor-status.js
+++ api/advisor-status.js
@@ -33,5 +33,9 @@ const paymentId = req.query.paymentId || "";
   console.log("Advisor call status:", dialCallStatus);
 
   res.setHeader("Content-Type", "text/xml");
-return res.status(200).send("<Response><Say>We are sorry. A ScamCheck advisor is not available right now. Please try again shortly.</Say><Hangup/></Response>");
+return res.status(200).send(
+  dialCallStatus === "completed"
+    ? "<Response><Hangup/></Response>"
+    : "<Response><Say>We are sorry. A ScamCheck advisor is not available right now. Please try again shortly.</Say><Hangup/></Response>"
+);
 }
