7a8743d5a0d55c8fa4eb70642f0c7235a7335e88
Megsmileface1 <hartmanstanley@gmail.com>
2026-09-19 11:50:56 -0400

    Temporarily set consultation payment to $1 for testing

--- api/create-payment.js
+++ api/create-payment.js
@@ -47,7 +47,7 @@ if (phone && !phone.startsWith("+")) {
           idempotency_key: idempotencyKey,
 
           amount_money: {
-            amount: 1000,
+            amount: 100,
             currency: "USD"
           },
 
